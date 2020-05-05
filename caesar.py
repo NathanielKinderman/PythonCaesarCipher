509da1f4c426f838a0ac16fc5fcaba52e4310357
natek <natekinderman16@gmail.com>
2020-05-05 14:40:22 -0500

    Wrote the function encryption to take a text and multiply by 2 and in a for loop for each letter that is in the valid_string variable take the letter by the index and put it in the output with the key as well

--- caesar.py
+++ caesar.py
@@ -1,1 +1,19 @@
-print('hello world')
+"""
+Python program is created to simulate a Caesar Ciper encryption/decryption text
+"""
+
+VALID_STRING = "abcdefghijklmnopqrstuvwxyz"
+
+def encryption(text,key):
+    #takes text and encrypt it by multiply the text by 2
+
+    encrypt_string = 'abcdefghiklmnopqrstuvwxyz' * 2 
+    output = ''
+
+    for char in VALID_STRING:
+        if char in VALID_STRING:
+            index = encrypt_string.index(str(char))
+            output += encrypt_string[index + key]
+        else:
+            output += char
+    return output
